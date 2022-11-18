5c0b921e8f1f39b62f58d6336e11cd5d0d9f0140
Calum Wilmot <calumwilmot@Calums-MacBook-Pro.local>
2022-11-18 11:34:40 +0000

    @cashflow and @transactions hashes functional

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,10 +1,3 @@
-class Statement
-    def initialize
-    end
-  
-    def statement_creator
-      @cashflow = @deposits.merge(@withdrawals)
-      @transactions = @cashflow.merge(@balance).sort 
-      return @transactions
-    end
+
+    
       
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,7 @@
 require './lib/account'
 
 account = Account.new
+account_2 = account.print_statement
 account_with_deposit = account.deposit(1000,'16-11-22')
 account_with_withdrawal = account.withdraw(500, '17-11-22')
 
@@ -18,7 +19,11 @@ RSpec.describe "account Class" do
     expect(account_with_deposit).to include('16-11-22' => 1000)
   end
 
-  it 'checks withdrawalss hash is correctly storing keys and values' do
+  it 'checks withdrawals hash is correctly storing keys and values' do
     expect(account_with_withdrawal).to include('17-11-22' => 500)
+  end
+
+  it 'checks print_statement is creating a functioning @transactions hash' do
+    expect(account_2).to include("15-11-22", 0)
   end
 end  
