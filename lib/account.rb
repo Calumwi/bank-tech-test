79c89dc0ca18b4e837d3daac8860821947117dd9
Calum Wilmot <calumwilmot@Calums-MacBook-Pro.local>
2022-11-18 17:05:49 +0000

    Completed working app

--- lib/account.rb
+++ lib/account.rb
@@ -1,5 +1,4 @@
 
-
 class Account
   def initialize
     @deposits = {}
@@ -7,7 +6,7 @@ class Account
     @transactions = {}
     @cashflow = {}
     @starting_balance = 0
-    @start_date = '15-11-22'
+    @start_date = '15-11-22 '
     @balance = {@start_date => @starting_balance}
   end
 
@@ -26,11 +25,23 @@ class Account
   def print_statement
     @cashflow = @deposits.merge(@withdrawals)
     @transactions = @cashflow.merge(@balance)
-    return @transactions.sort.flatten
+    @transactions.sort.flatten
+
+    puts "date || credit || debit || balance"
+    balance = 0
+    @transactions.each do |key, value|
+        if value < 0
+            balance += value
+            puts "#{key} || || #{value} || #{balance} ||\n"
+        else
+            balance += value
+            puts "#{key} || #{value} || || #{balance} ||\n"
+        end
+    end
   end
 end
 
 account = Account.new
-account.deposit(1000,'16-11-22')
-account.withdraw(500, '17-11-22')
+account.deposit(1000,'16-11-22 ')
+account.withdraw(500, '17-11-22 ')
 account.print_statement
--- spec/statement_spec.rb
+++
@@ -1,7 +0,0 @@
-# require_relative "../spec/account_spec.rb"
-
-# statement = Statement.new
-# RSpec.describe "Statement Class" do
-#   it "returns transactions hash" do
-# end
-# end
